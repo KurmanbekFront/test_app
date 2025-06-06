48d5264d7284736dc8b35e37a9a845f3e7baab7d
KurmanbekFront <nurkuluulukurmanbek53@gmail.com>
2025-06-06 20:04:53 +0600

    add zustand and react query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,27 @@
-import { CustomButton } from "shared/custombutton";
+import { CustomButton } from "shared/ui/custombutton";
 
 function App() {
   return (
     <>
-      <CustomButton children="Кнопка" />
+      <CustomButton children="Кнопка 1" type="button" />
+      <CustomButton
+        children="Кнопка 2"
+        type="submit"
+        size="sm"
+        variant="secondary"
+      />
+      <CustomButton
+        children="Кнопка 3"
+        type="reset"
+        size="md"
+        variant="danger"
+      />
+      <CustomButton
+        children="Кнопка 4"
+        type="button"
+        size="lg"
+        disabled={true}
+      />
     </>
   );
 }
